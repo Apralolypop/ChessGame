9810974b50ed39d8e878c07487a24d96a0a99368
Tony Wang <tony.wang.4070@gmail.com>
2026-07-12 14:28:05 -0400

    worker 2 test

--- board.cpp
+++ board.cpp
@@ -8,7 +8,10 @@
 //new branch new test wowowow NETAN1
 
 std::unique_ptr<Piece> board[8][8];
-//"NEW AAH VARIABLE"
+
+/*"NEW AAH VARIABLE"
+new variable trust
+*/
 
 std::vector<int> KingPositionWhite = {0, 4}; // Initial position of the white king
 std::vector<int> KingPositionBlack = {7, 4}; // Initial position of the black king
